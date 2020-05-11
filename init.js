eac43b76fbb5fed5f3276328af626b96200de7aa
7702244 <56172899+7702244@users.noreply.github.com>
2020-05-11 23:15:01 +0300

    Update init.js

--- init.js
+++ init.js
@@ -1,33 +1,17 @@
-var LVM = (function (LVM) {
-    "use strict";
+"use strict";
 
-    /* ----------------------------------------------------------- */
-    /* == initiator */
-    /* ----------------------------------------------------------- */
+(function (w, d, p) {
+    d.addEventListener("DOMContentLoaded", function () {
+        var cs = d.currentScript || d.scripts[d.scripts.length - 1];
+        for (var i = 0; i < w.wreg.widgets.length; i++) {
+            var wUrl = w.wreg.widgets[i][1] === 'contactform' ? 'forms/widget.contactform.min.js' : 'widget.js'
 
-    LVM.initiator = function (promoId, options) {
-      options = options || {};
-      
-      (function(d, w, p) {
-          var cs = d.currentScript || d.scripts[d.scripts.length - 1];
-          (function a(u, c) {
-              var s = d.createElement('script');
-              s.type = 'text/javascript';
-              s.async = !0;
-              s.charset = 'utf-8';
-              s.src = u;
-              s.onerror = c ? function() {
-                  a(c)
-              } : null;
-              cs.parentNode.insertBefore(s, cs)
-          })(w, p + '?url=' + encodeURIComponent(w))
-      })(document, 'https://cdn.slon.biz/widget.js?promoId=' + promoId, '/proxy.php');
-    };
-
-    /* ----------------------------------------------------------- */
-    /* == return */
-    /* ----------------------------------------------------------- */
-
-    return LVM;
-
-}(LVM || {}));
+            var s = d.createElement('script');
+            s.type = 'text/javascript';
+            s.async = 1;
+            s.charset = 'utf-8';
+            s.src = p + '?url=' + encodeURIComponent('https://cdn.slon.biz/' + wUrl + '?promoId=' + w.wreg.widgets[i][0]);
+            cs.parentNode.insertBefore(s, cs)
+        }
+    });
+})(window, document, 'https://tbpage.com/proxy.php');
