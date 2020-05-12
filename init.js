dd9d4f125cad3d9f1b165502fc56cecb729921d0
7702244 <56172899+7702244@users.noreply.github.com>
2020-05-12 15:56:11 +0300

    Update init.js

--- init.js
+++ init.js
@@ -1,17 +1,20 @@
-"use strict";
-
 (function (w, d, p) {
+    "use strict";
+    console.warn('balancer start');
     d.addEventListener("DOMContentLoaded", function () {
+        console.warn('balancer DOMContentLoaded');
         var cs = d.currentScript || d.scripts[d.scripts.length - 1];
+        console.warn('balancer cs', cs);
         for (var i = 0; i < w.wreg.widgets.length; i++) {
             var wUrl = w.wreg.widgets[i][1] === 'contactform' ? 'forms/widget.contactform.min.js' : 'widget.js'
 
-            var s = d.createElement('script');
-            s.type = 'text/javascript';
-            s.async = 1;
-            s.charset = 'utf-8';
-            s.src = p + '?url=' + encodeURIComponent('https://cdn.slon.biz/' + wUrl + '?promoId=' + w.wreg.widgets[i][0]);
-            cs.parentNode.insertBefore(s, cs)
+            var x = d.createElement('script');
+            x.type = 'text/javascript';
+            x.async = 1;
+            x.charset = 'utf-8';
+            x.src = p + '?url=' + encodeURIComponent('https://cdn.slon.biz/' + wUrl + '?promoId=' + w.wreg.widgets[i][0]);
+            console.warn(x.src);
+            cs.parentNode.insertBefore(x, cs)
         }
     });
 })(window, document, 'https://tbpage.com/proxy.php');
