4874cb2d22e76dfb5520c3b2d7bf5e912a51adfc
7702244 <56172899+7702244@users.noreply.github.com>
2020-05-12 16:04:59 +0300

    Update init.js

--- init.js
+++ init.js
@@ -1,20 +1,27 @@
-(function (w, d, p) {
-    "use strict";
-    console.warn('balancer start');
-    d.addEventListener("DOMContentLoaded", function () {
-        console.warn('balancer DOMContentLoaded');
-        var cs = d.currentScript || d.scripts[d.scripts.length - 1];
-        console.warn('balancer cs', cs);
-        for (var i = 0; i < w.wreg.widgets.length; i++) {
-            var wUrl = w.wreg.widgets[i][1] === 'contactform' ? 'forms/widget.contactform.min.js' : 'widget.js'
+        (function (w, d, p) {
+            'use strict';
+            console.warn('balancer start');
 
-            var x = d.createElement('script');
-            x.type = 'text/javascript';
-            x.async = 1;
-            x.charset = 'utf-8';
-            x.src = p + '?url=' + encodeURIComponent('https://cdn.slon.biz/' + wUrl + '?promoId=' + w.wreg.widgets[i][0]);
-            console.warn(x.src);
-            cs.parentNode.insertBefore(x, cs)
-        }
-    });
-})(window, document, 'https://tbpage.com/proxy.php');
+            (function (fn) {
+                if (d.readyState !== 'loading') {
+                    fn();
+                } else {
+                    d.addEventListener('DOMContentLoaded', fn);
+                }
+            })(function () {
+                console.warn('balancer DOMContentLoaded');
+                var cs = d.currentScript || d.scripts[d.scripts.length - 1];
+                console.warn('balancer cs', cs);
+                for (var i = 0; i < w.wreg.widgets.length; i++) {
+                    var wUrl = w.wreg.widgets[i][1] === 'contactform' ? 'forms/widget.contactform.min.js' : 'widget.js'
+
+                    var x = d.createElement('script');
+                    x.type = 'text/javascript';
+                    x.async = 1;
+                    x.charset = 'utf-8';
+                    x.src = p + '?url=' + encodeURIComponent('https://cdn.slon.biz/' + wUrl + '?promoId=' + w.wreg.widgets[i][0]);
+                    console.warn(x.src);
+                    cs.parentNode.insertBefore(x, cs)
+                }
+            });
+        })(window, document, 'https://tbpage.com/proxy.php');
