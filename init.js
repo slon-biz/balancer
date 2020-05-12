7270be2d3742b5d310f9066c776a7098efa1c169
7702244 <56172899+7702244@users.noreply.github.com>
2020-05-12 19:06:36 +0300

    Update init.js

--- init.js
+++ init.js
@@ -8,16 +8,15 @@
             d.addEventListener('DOMContentLoaded', fn);
         }
     })(function () {
-        var cs = d.currentScript || d.scripts[d.scripts.length - 1];
-
         for (var i = 0; i < w.wreg.widgets.length; i++) {
-            var wUrl = w.wreg.widgets[i][1] === 'contactform' ? 'forms/widget.contactform.min.js' : 'widget.js'
+            var cs = w.wreg.widgets[i][0];
+            var wUrl = w.wreg.widgets[i][2] === 'contactform' ? 'forms/widget.contactform.min.js' : 'widget.js'
 
             var x = d.createElement('script');
             x.type = 'text/javascript';
             x.async = 1;
             x.charset = 'utf-8';
-            x.src = p + '?url=' + encodeURIComponent('https://slon.biz/' + wUrl + '?promoId=' + w.wreg.widgets[i][0]);
+            x.src = p + '?url=' + encodeURIComponent('https://slon.biz/' + wUrl + '?promoId=' + w.wreg.widgets[i][1]);
 
             cs.parentNode.insertBefore(x, cs)
         }
