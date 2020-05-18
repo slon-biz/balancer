367b4cbcaaea830437330497bcab9ec50929dd12
7702244 <56172899+7702244@users.noreply.github.com>
2020-05-18 09:36:47 +0300

    Update init.js

--- init.js
+++ init.js
@@ -10,10 +10,9 @@
     })(function () {
         for (var i = 0; i < w.wreg.widgets.length; i++) {
             var x = d.createElement('script');
-            x.type = 'text/javascript';
             x.async = 1;
             x.charset = 'utf-8';
-            x.src = p + '?url=' + encodeURIComponent('https://slon.biz/forms/' + w.wreg.widgets[i][2] + '.min.js?promoId=' + w.wreg.widgets[i][1]);
+            x.src = p + '?url=' + encodeURIComponent('https://slon.biz/forms/widget.min.js?promoId=' + w.wreg.widgets[i][1]);
 
             var cs = w.wreg.widgets[i][0];
             cs.parentNode.insertBefore(x, cs)
